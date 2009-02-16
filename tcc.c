c3e26ba433942a10a044ffd0650bf021684761a3
grischka <grischka>
2009-02-16 15:11:45 +0100

    i386: align=8 for double and long long

--- tcc.c
+++ tcc.c
@@ -5390,7 +5390,7 @@ int type_size(CType *type, int *a)
         return LDOUBLE_SIZE;
     } else if (bt == VT_DOUBLE || bt == VT_LLONG) {
 #ifdef TCC_TARGET_I386
-        *a = 4;
+        *a = 8;
 #elif defined(TCC_TARGET_ARM)
 #ifdef TCC_ARM_EABI
         *a = 8; 
